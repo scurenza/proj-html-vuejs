4eed718f449b0e0f1f758c79d89e444049bc89c3
scurenza <fedechiar@hotmail.it>
2022-11-19 23:06:14 +0100

    layout card completo

---
+++ src/components/AppCard.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+    name: "AppCard",
+    props: {
+        cards: Object
+    },
+    methods: {
+        getImagePath: function(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card-container">
+        <div class="image">
+            <img src="../assets/images/1109398_4c13-272x161.jpg" alt="">
+        </div>
+        <div class="card-description">
+            <div class="category mt-1">
+                <span> Development ></span>
+            </div>
+            <div class="card-text mt-2 pb-3">
+                the complete iOS 10 & Swift 3 Developer Course 
+            </div>
+            <div class="card-bottom mt-3 d-flex justify-content-between align-items-center">
+                <div class="vote">
+                    <img src="../assets/images/starfull.svg" alt="">
+                    <img src="../assets/images/starfull.svg" alt="">
+                    <img src="../assets/images/starfull.svg" alt="">
+                    <img src="../assets/images/starfull.svg" alt="">
+                    <img src="../assets/images/staremptyl.svg" alt="">
+                </div>
+                <div class="price d-flex flex-column">
+                    <span class="original-price">$150</span>
+                    <span class="discount">$25</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.card-container {
+    min-height: 350px;
+    width: calc(100% / 6 - 10px);
+    margin: 20px 0;
+
+    border: 1px solid rgba(128, 128, 128, 0.459);
+
+    .card-description {
+        padding: 1.2rem;
+        
+        .category span {
+            color: grey;
+            font-size: .8rem;
+        }
+        .card-text {
+            border-bottom: 1px solid grey;
+        }
+
+        .price{
+            .original-price {
+                text-decoration: line-through;
+                color: grey;
+                font-size: .75rem;
+            }
+
+            .discount {
+                font-size: 1.1rem;
+                font-weight: 600;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/AppListTop.vue
@@ -0,0 +1,41 @@
+<script>
+import AppCard from './AppCard.vue';
+
+export default {
+    name: "AppListTop",
+    props: {
+        cards: Object
+    },
+    components: { AppCard }
+}
+</script>
+
+<template>
+    <section class="app-list-top">
+        <div class="my-container">
+            <h4 class="title">Popular Development Courses</h4>
+            <div class="card-row d-flex flex-wrap justify-content-between">
+                <AppCard  v-for="n in 6"/>
+                <AppCard  v-for="n in 6"/>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.app-list-top {
+    margin: 2rem;
+
+    .title {
+        font-weight: 600;
+        margin-top: 4rem;
+    }
+
+    .row-cols-6 {
+        margin-top: 2rem;
+    }
+}
+
+</style>
